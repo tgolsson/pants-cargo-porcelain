eeaa56bd5789458b08fa0a8881a50e873fdb6f16
Tom Solberg <tom.solberg@embark-studios.com>
2023-10-24 23:58:34 +0200

    do not treat workspace as package

--- pants-plugins/cargo-porcelain/pants_cargo_porcelain/goals/tailor.py
+++ pants-plugins/cargo-porcelain/pants_cargo_porcelain/goals/tailor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from dataclasses import dataclass
 
 from pants.core.goals.tailor import (
@@ -8,8 +9,8 @@ from pants.core.goals.tailor import (
     PutativeTargets,
     PutativeTargetsRequest,
 )
-from pants.engine.fs import PathGlobs, Paths
-from pants.engine.internals.selectors import Get, MultiGet
+from pants.engine.fs import DigestContents, PathGlobs
+from pants.engine.internals.selectors import Get
 from pants.engine.rules import collect_rules, rule
 from pants.engine.unions import UnionRule
 from pants.util.dirutil import group_by_dir
@@ -17,7 +18,6 @@ from pants.util.logging import LogLevel
 
 from pants_cargo_porcelain.subsystems import RustSubsystem, RustupTool
 from pants_cargo_porcelain.target_types import CargoPackageTarget
-from pants_cargo_porcelain.util_rules.rustup import RustToolchain, RustToolchainRequest
 
 
 @dataclass(frozen=True)
@@ -35,27 +35,25 @@ async def find_putative_targets(
     if not rust.tailor:
         return PutativeTargets()
 
-    all_cargo_files, toolchain = await MultiGet(
-        Get(Paths, PathGlobs, req.path_globs("Cargo.toml")),
-        Get(
-            RustToolchain,
-            RustToolchainRequest("1.72.1", "x86_64-unknown-linux-gnu", ("cargo", "clippy")),
-        ),
-    )
+    all_cargo_contents = await Get(DigestContents, PathGlobs, req.path_globs("Cargo.toml"))
 
-    unowned_cargo_files = set(all_cargo_files.files) - set(all_owned_sources)
+    unowned_cargo_files = set(fc.path for fc in all_cargo_contents) - set(all_owned_sources)
+    unowned_cargo_contents = {
+        fc.path: fc.content for fc in all_cargo_contents if fc.path in unowned_cargo_files
+    }
 
     pts = []
 
     for dirname, filenames in group_by_dir(unowned_cargo_files).items():
-        pts.append(
-            PutativeTarget.for_target_type(
-                CargoPackageTarget,
-                path=dirname,
-                name=None,
-                triggering_sources=sorted(filenames),
+        if b"[package]" in unowned_cargo_contents[os.path.join(dirname, "Cargo.toml")]:
+            pts.append(
+                PutativeTarget.for_target_type(
+                    CargoPackageTarget,
+                    path=dirname,
+                    name=None,
+                    triggering_sources=sorted(filenames),
+                )
             )
-        )
 
     return PutativeTargets(pts)
 
