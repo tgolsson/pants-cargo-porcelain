d7e2f8a23265c610795d9b5f17a8cac08dec1973
Tom Solberg <me@sbg.dev>
2023-10-28 00:11:25 +0200

    use file locks for installing rust toolchain (#5)

--- pants-plugins/cargo-porcelain/pants_cargo_porcelain/util_rules/rustup.py
+++ pants-plugins/cargo-porcelain/pants_cargo_porcelain/util_rules/rustup.py
@@ -1,5 +1,8 @@
+import fcntl
+import os
 from dataclasses import dataclass
 
+from pants.base.build_environment import get_pants_cachedir
 from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
 from pants.engine.fs import EMPTY_DIGEST
 from pants.engine.platform import Platform
@@ -74,67 +77,97 @@ async def get_rustup_binary(
     return RustupBinary(path=f"{CARGO_NAMED_CACHE}/bin/rustup")
 
 
+class FileLock:
+    def __init__(self, path: str):
+        directory = os.path.dirname(path)
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+
+        open_mode = os.O_RDWR | os.O_CREAT | os.O_TRUNC
+        fd = os.open(path, open_mode)
+        self._fd = fd
+
+    def __enter__(self):
+        fcntl.flock(self._fd, fcntl.LOCK_EX)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._fd:
+            fcntl.flock(self._fd, fcntl.LOCK_UN)
+            os.close(self._fd)
+
+        self._fd = None
+
+
 @rule(desc="Get Rust toolchain", level=LogLevel.DEBUG)
 async def get_rust_toolchain(request: RustToolchainRequest) -> RustToolchain:
     rustup_binary = await Get(RustupBinary, RustupBinaryRequest())
-
-    _ = await Get(
-        ProcessResult,
-        Process(
-            argv=[rustup_binary.path, "toolchain", "install", request.version],
-            input_digest=EMPTY_DIGEST,
-            description=f"Installing Rust {request.version}",
-            level=LogLevel.DEBUG,
-            append_only_caches=BOTH_CACHES,
-            env={"RUSTUP_HOME": RUSTUP_NAMED_CACHE, "CARGO_HOME": CARGO_NAMED_CACHE},
-        ),
-    )
-
-    _ = await Get(
-        ProcessResult,
-        Process(
-            argv=[
-                rustup_binary.path,
-                "target",
-                "add",
-                f"--toolchain={request.version}",
-                request.target,
-            ],
-            input_digest=EMPTY_DIGEST,
-            description=f"Installing target {request.target} for toolchain {request.version}",
-            level=LogLevel.DEBUG,
-            append_only_caches=BOTH_CACHES,
-            env={"RUSTUP_HOME": RUSTUP_NAMED_CACHE, "CARGO_HOME": CARGO_NAMED_CACHE},
-        ),
-    )
-
-    if request.components:
+    lock_file_path = os.path.join(get_pants_cachedir(), "locks", ".rustup")
+    with FileLock(lock_file_path):
         _ = await Get(
             ProcessResult,
             Process(
                 argv=[
                     rustup_binary.path,
-                    "component",
-                    "add",
-                    f"--toolchain={request.version}",
-                    *request.components,
+                    "toolchain",
+                    "install",
+                    "--no-self-update",
+                    request.version,
                 ],
                 input_digest=EMPTY_DIGEST,
-                description=(
-                    f"Installing components {request.components} for toolchain {request.version}"
-                ),
+                description=f"Installing Rust {request.version}",
                 level=LogLevel.DEBUG,
                 append_only_caches=BOTH_CACHES,
                 env={"RUSTUP_HOME": RUSTUP_NAMED_CACHE, "CARGO_HOME": CARGO_NAMED_CACHE},
             ),
         )
 
-    return RustToolchain(
-        path=f"{RUSTUP_NAMED_CACHE}/toolchains/{request.version}-{request.target}",
-        version=request.version,
-        target=request.target,
-        ok=True,
-    )
+        _ = await Get(
+            ProcessResult,
+            Process(
+                argv=[
+                    rustup_binary.path,
+                    "target",
+                    "add",
+                    f"--toolchain={request.version}",
+                    request.target,
+                ],
+                input_digest=EMPTY_DIGEST,
+                description=f"Installing target {request.target} for toolchain {request.version}",
+                level=LogLevel.DEBUG,
+                append_only_caches=BOTH_CACHES,
+                env={"RUSTUP_HOME": RUSTUP_NAMED_CACHE, "CARGO_HOME": CARGO_NAMED_CACHE},
+            ),
+        )
+
+        if request.components:
+            _ = await Get(
+                ProcessResult,
+                Process(
+                    argv=[
+                        rustup_binary.path,
+                        "component",
+                        "add",
+                        f"--toolchain={request.version}",
+                        *request.components,
+                    ],
+                    input_digest=EMPTY_DIGEST,
+                    description=(
+                        f"Installing components {request.components} for toolchain"
+                        f" {request.version}"
+                    ),
+                    level=LogLevel.DEBUG,
+                    append_only_caches=BOTH_CACHES,
+                    env={"RUSTUP_HOME": RUSTUP_NAMED_CACHE, "CARGO_HOME": CARGO_NAMED_CACHE},
+                ),
+            )
+
+        return RustToolchain(
+            path=f"{RUSTUP_NAMED_CACHE}/toolchains/{request.version}-{request.target}",
+            version=request.version,
+            target=request.target,
+            ok=True,
+        )
 
 
 def rules():
